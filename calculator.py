ce560d1fc8e1881c5f1a391d8e0d2e0936eae8c6
woranoot.kampil@gmail.com <woranoot.kampil@gmail.com>
2026-09-15 10:20:54 +0700

    add function add()

---
+++ calculator.py
@@ -0,0 +1,18 @@
+
+def add(x,y):
+    return x + y
+def substract(x,y):
+    return x - y
+def multiply(x,y):
+    return x * y
+def division(x,y):
+    return x / y
+
+
+# ----- Main Function -----#
+print ('Simple Calculator')
+num1 = float(input('กรอกเลขตัวที่1'))
+num2 = float(input('กรอกเลขตัวที่2'))
+
+print("-"*25)
+print("Addition (+): ", add(num1,num2))
